6f25f3b65953cca98ee43c7b669b53e51a2699de
AymaneSab <88624531+AymaneSab@users.noreply.github.com>
2023-11-01 16:43:30 +0100

    Project Setup and Environment Fixes #1Update Customer APi

--- api/customer_api/main.py
+++ api/customer_api/main.py
@@ -1,21 +1,20 @@
-# api/customer_api/main.py
-
-import os 
-import sys 
+import sys
+import time
 from flask import Flask, jsonify
 
-sys.path.append('/Users/sabri/Desktop/Study /Youcode/Github/Sprint_4/fraud-detection-system/api') 
+sys.path.append('/Users/sabri/Desktop/Study /Youcode/Github/Sprint_4/fraud-detection-system/api/') 
 
-from generated_data import customers
-
-
-
+from generated_data import customers  
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-@app.route('/api/customers', methods=['GET'])
-def get_customers():
-    return jsonify(customers)
+def json_entry():
+    for customer in customers:
+        yield jsonify(customer)
+        time.sleep(2)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
+
+
