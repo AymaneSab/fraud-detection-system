1cd4451afa6478ee42e6670018472932d1bb5ac8
AymaneSab <88624531+AymaneSab@users.noreply.github.com>
2023-11-27 16:42:12 +0100

    Validate external_api

--- api/external_api/main.py
+++ api/external_api/main.py
@@ -7,7 +7,7 @@ sys.path.append('/Users/sabri/Desktop/Study /Youcode/Github/Sprint_4/fraud-detec
 
 from generated_data import external_data  
 
-# Load the schema from customer_schema.json
+# Load the schema from external_schema.json
 with open('/Users/sabri/Desktop/Study /Youcode/Github/Sprint_4/fraud-detection-system/api/external_api/external_schema.json') as schema_file:
     external_schema = json.load(schema_file)
 
@@ -21,10 +21,16 @@ def validate_external(external):
         print(f"Schema Validation Error: {e}")
         return False
 
+def validate_external_list(external_data):
+    if validate_external(external_data):
+        return [external_data]
+    else:
+        return []
+
 @app.route('/api/external', methods=['GET'])
 def json_entry():
-    valid_external = [external for external in external_data if validate_external(external)]
-    return jsonify(valid_external)
+    validated_external = validate_external_list(external_data)
+    return jsonify(validated_external)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
