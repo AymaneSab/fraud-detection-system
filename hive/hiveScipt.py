622d1bb1b9cdf9592e69164c796971ff1afec0f8
AymaneSab <sabriaymane59@gmail.com>
2023-12-01 10:40:33 +0100

    Fix Connection With Hive

--- hive/hiveScipt.py
+++ hive/hiveScipt.py
@@ -1,16 +1,63 @@
 from pyhive import hive
 import json
+import logging
+import os
+from datetime import datetime
 
-# Connect to Hive
-conn = hive.Connection(host='your_hive_host', port=10000, username='your_username')
 
-# Function to execute Hive queries
-def execute_hive_query(query):
-    with conn.cursor() as cursor:
-        cursor.execute(query)
+def setup_logging(log_directory, logger_name):
+    os.makedirs(log_directory, exist_ok=True)
+
+    log_filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")
+    log_filepath = os.path.join(log_directory, log_filename)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    handler = logging.FileHandler(log_filepath)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
+
+# Function to connect To Hive 
+def connect_to_hive(host, port, conn_logger):
+    try:
+        conn = hive.Connection(host=host, port=port)
+        conn_logger.info("Connection successful to Hive")
+        return conn
+    except Exception as e:
+        conn_logger.error(f"Connection failed to Hive\nError: {str(e)}")
+        raise  # Re-raise the exception to halt the script
+
+def execute_hive_query(query, conn, query_logger):
+    try:
+        with conn.cursor() as cursor:
+            if cursor.execute(query):
+                query_logger.info("Query successful to Hive")
+            else:
+                query_logger.error("Failed query to Hive")
+                raise Exception(f"Hive query failed: {query}")
+    except Exception as e:
+        query_logger.error(f"Exception detected in Hive query: {str(e)}")
+        raise  # Re-raise the exception to halt the script
+
+# Function to create a Hive database
+def create_hive_database(database_name, conn, database_conn_logger):
+    try:
+        create_database_query = f"CREATE DATABASE IF NOT EXISTS {database_name};"
+        database_conn_logger.info(create_database_query)
+        execute_hive_query(create_database_query, conn, database_conn_logger)
+        database_conn_logger.info(f"Database {database_name} created successfully.")
+    except Exception as e:
+        database_conn_logger.error(f"Failed to create Hive database\nError: {str(e)}")
+        raise  # Re-raise the exception to halt the script
 
 # Function to create Hive table based on JSON schema
-def create_hive_table(table_name, json_schema):
+def create_hive_table(database_name, table_name, json_schema, conn, query_logger):
     # Generate Hive data types based on JSON schema
     hive_data_types = {
         "string": "STRING",
@@ -34,26 +81,40 @@ def create_hive_table(table_name, json_schema):
     columns = generate_columns(json_schema)
 
     # Create Hive table query
-    create_table_query = f"CREATE TABLE {table_name} ({', '.join(columns)}) STORED AS ORC;"
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {database_name}.{table_name} ({', '.join(columns)}) STORED AS ORC;"
 
     # Execute the query
-    execute_hive_query(create_table_query)
-    print(f"Table {table_name} created successfully.")
+    execute_hive_query(create_table_query, conn, query_logger)
+    query_logger.info(f"Table {database_name}.{table_name} created successfully.")
 
 # Read schemas from JSON files
-with open('customers_schema.json', 'r') as file:
+with open('/home/hdoop/fraud-detection-system/hive/schemas/customer_schema.json', 'r') as file:
     customers_schema = json.load(file)
 
-with open('externals_schema.json', 'r') as file:
+with open('/home/hdoop/fraud-detection-system/hive/schemas/external_schema.json', 'r') as file:
     externals_schema = json.load(file)
 
-with open('transactions_schema.json', 'r') as file:
+with open('/home/hdoop/fraud-detection-system/hive/schemas/transaction_schema.json', 'r') as file:
     transactions_schema = json.load(file)
 
-# Create Hive tables for Customers, Externals, and Transactions
-create_hive_table("customers", customers_schema)
-create_hive_table("externals", externals_schema)
-create_hive_table("transactions", transactions_schema)
+conn_logger = setup_logging("Log/Hive/Conn_Log_Files", "Hive_Connection")
+query_logger = setup_logging("Log/Hive/Query_Log_Files", "Hive_Query")
+database_conn_logger = setup_logging("Log/Hive/Database_Log_Files", "Hive_Database_Creation")
 
-# Close the Hive connection
-conn.close()
+hive_database_name = "transactions"
+host = "localhost"
+port = 10001
+
+try:
+    conn = connect_to_hive(host, port, conn_logger)
+
+    # Create the Hive database
+    create_hive_database(hive_database_name, conn, database_conn_logger)
+
+    # Create Hive tables for Customers, Externals, and Transactions
+    create_hive_table(hive_database_name, "customers", customers_schema, conn, query_logger)
+    create_hive_table(hive_database_name, "externals", externals_schema, conn, query_logger)
+    create_hive_table(hive_database_name, "transactions", transactions_schema, conn, query_logger)
+
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
